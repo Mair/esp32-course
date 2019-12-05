ebc20295908ff8276df5067127074da76bc487d2
mairs <mair.swartz@live.com>
2019-12-05 22:04:58 +1100

    added sleep

--- _13_Internet_Connection/_13_1_exampleConnect_to_del/main/main.c
+++ _13_Internet_Connection/_13_1_exampleConnect_to_del/main/main.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+#include <string.h>
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
-#include "esp_http_client.h"
 #include "protocol_examples_common.h"
+#include "esp_http_client.h"
 
-esp_err_t clientEvent(esp_http_client_event_t *evt)
+esp_err_t clientEventHandler(esp_http_client_event_t *evt)
 {
   switch (evt->event_id)
   {
   case HTTP_EVENT_ON_DATA:
     printf("%.*s\n", evt->data_len, (char *)evt->data);
-    break;
   default:
     break;
   }
@@ -20,16 +19,16 @@ esp_err_t clientEvent(esp_http_client_event_t *evt)
 
 void app_main(void)
 {
-  nvs_flash_init();
+  ESP_ERROR_CHECK(nvs_flash_init());
   tcpip_adapter_init();
-  esp_event_loop_create_default();
-  example_connect();
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  ESP_ERROR_CHECK(example_connect());
 
   esp_http_client_config_t clientConfig = {
       .url = "http://google.com",
-      .event_handler = clientEvent};
+      .event_handler = clientEventHandler};
 
   esp_http_client_handle_t client = esp_http_client_init(&clientConfig);
-  esp_http_client_perform(client);
+  esp_err_t err = esp_http_client_perform(client);
   esp_http_client_cleanup(client);
 }
