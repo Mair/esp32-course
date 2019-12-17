aa74b33a12983286de64440b35719e89e7f4ae76
mairs <mair.swartz@live.com>
2019-12-17 22:02:25 +1100

    EXT 1 and hybranation

---
+++ _12_sleeping/_6_hybranation_timer/main/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_sleep.h"
+
+RTC_DATA_ATTR int timesWokenUp = 0;
+void app_main(void)
+{
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_enable_timer_wakeup(5 * 1000000);
+  printf("going to sleep. woken up %d\n", timesWokenUp++);
+
+  esp_deep_sleep_start();
+}
