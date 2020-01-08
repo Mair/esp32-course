618a44f5b86f373a4a1070069ba0035a55581533
mairs <mair.swartz@live.com>
2020-01-08 20:27:08 +1100

    OTA ADVANCED

--- _18_OTA/_18_3_OTA_using_google_drive_ota_advanced/main/main.c
+++ _18_OTA/_18_3_OTA_using_google_drive_ota_advanced/main/main.c
@@ -22,16 +22,33 @@ esp_err_t client_event_handler(esp_http_client_event_t *evt)
   return ESP_OK;
 }
 
+esp_err_t validate_image_header(esp_app_desc_t *incoming_ota_desc)
+{
+  const esp_partition_t *running_partition = esp_ota_get_running_partition();
+  esp_app_desc_t running_partition_description;
+  esp_ota_get_partition_description(running_partition, &running_partition_description);
+
+  ESP_LOGI(TAG, "current version is %s\n", running_partition_description.version);
+  ESP_LOGI(TAG, "new version is %s\n", incoming_ota_desc->version);
+
+  if (strcmp(running_partition_description.version, incoming_ota_desc->version) == 0)
+  {
+    ESP_LOGW(TAG, "NEW VERSION IS THE SAME AS CURRENT VERSION. ABORTING");
+    return ESP_FAIL;
+  }
+  return ESP_OK;
+}
+
 void run_ota(void *params)
 {
+  ESP_ERROR_CHECK(nvs_flash_init());
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
   while (true)
   {
     xSemaphoreTake(ota_semaphore, portMAX_DELAY);
     ESP_LOGI(TAG, "Invoking OTA");
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 
     esp_http_client_config_t clientConfig = {
@@ -39,7 +56,49 @@ void run_ota(void *params)
         .event_handler = client_event_handler,
         .cert_pem = (char *)server_cert_pem_start};
 
-    if (esp_https_ota(&clientConfig) == ESP_OK)
+    esp_https_ota_config_t ota_config = {
+        .http_config = &clientConfig};
+
+    esp_https_ota_handle_t ota_handle = NULL;
+
+    if (esp_https_ota_begin(&ota_config, &ota_handle) != ESP_OK)
+    {
+      ESP_LOGE(TAG, "esp_https_ota_begin failed");
+      example_disconnect();
+      continue;
+    }
+
+    esp_app_desc_t incoming_ota_desc;
+    if (esp_https_ota_get_img_desc(ota_handle, &incoming_ota_desc) != ESP_OK)
+    {
+      ESP_LOGE(TAG, "esp_https_ota_get_img_desc failed");
+      esp_https_ota_finish(ota_handle);
+      example_disconnect();
+      continue;
+    }
+
+    if (validate_image_header(&incoming_ota_desc) != ESP_OK)
+    {
+      ESP_LOGE(TAG, "validate_image_header failed");
+      esp_https_ota_finish(ota_handle);
+      example_disconnect();
+      continue;
+    }
+
+    while (true)
+    {
+      esp_err_t ota_result = esp_https_ota_perform(ota_handle);
+      if (ota_result != ESP_ERR_HTTPS_OTA_IN_PROGRESS)
+        break;
+    }
+
+    if (esp_https_ota_finish(ota_handle) != ESP_OK)
+    {
+      ESP_LOGE(TAG, "esp_https_ota_finish failed");
+      example_disconnect();
+      continue;
+    }
+    else
     {
       printf("restarting in 5 seconds\n");
       vTaskDelay(pdMS_TO_TICKS(5000));
