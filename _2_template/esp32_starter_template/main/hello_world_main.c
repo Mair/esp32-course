c27a0048fce03ddf26d7ce3f82a8c0ce16c0eb29
Mair <mair.swartz@live.com>
2021-11-02 11:17:44 +1100

    merge

---
+++ _2_template/esp32_starter_template/main/hello_world_main.c
@@ -0,0 +1,19 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+}
