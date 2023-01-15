5514099a2932f10bc3d15bcb32077f74580d738f
mair <mair.swartz@live.com>
2023-01-15 16:38:27 +1100

    random generator v5

--- _3_first_look/_2_3_random_generator-dice/main/main.c
+++ _3_first_look/_2_3_random_generator-dice/main/main.c
@@ -2,17 +2,18 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
+// for idf version 4.x use #include "esp_system.h"
+#include "esp_random.h"
 #include "math.h"
 
 #define TAG "DICE"
 
 int dice_role()
 {
-    int random = esp_random();
-    int positiveNumber = abs(random);
-    int diceNumber = (positiveNumber % 6) + 1;
-    return diceNumber;
+  int random = esp_random();
+  int positiveNumber = abs(random);
+  int diceNumber = (positiveNumber % 6) + 1;
+  return diceNumber;
 }
 
 void app_main(void)
@@ -22,5 +23,4 @@ void app_main(void)
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     ESP_LOGI(TAG, "random number %d", dice_role());
   }
-  
 }
